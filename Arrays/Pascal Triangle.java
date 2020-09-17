57653cbb45f5473d72b32448184b4e4d5aee4d1b
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-09-17 23:59:21 +0530

    Add files via upload

---
+++ Arrays/Pascal Triangle.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> solve(int A) 
+    {
+        if(A==0)
+        {
+            return new ArrayList<>();
+        }
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(1);
+        ans.add(a);
+        for(int i=1;i<A;i++)
+        {
+            a=new ArrayList<>();
+            for(int j=0;j<i;j++)
+            {
+                if(j==0)
+                {
+                    a.add(1);
+                }
+                else
+                {
+                    a.add(ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+                }
+            }
+            a.add(1);
+            ans.add(a);
+        }
+        return ans;
+    }
+}
