06d91ec6a8b23e36771b8cc2356f1759939089ec
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-09-20 22:21:56 +0530

    Add files via upload

--- Arrays/Kth Row of Pascal's Triangle.java
+++ Arrays/Kth Row of Pascal's Triangle.java
@@ -1,27 +1,13 @@
 public class Solution {
     public ArrayList<Integer> getRow(int A) 
     {
-        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
-        ArrayList<Integer> a=new ArrayList<>();
-        a.add(1);
-        ans.add(a);
+        ArrayList<Integer> ans=new ArrayList<>();
+        ans.add(1);
         for(int i=1;i<=A;i++)
         {
-            a=new ArrayList<>();
-            for(int j=0;j<i;j++)
-            {
-                if(j==0)
-                {
-                    a.add(1);
-                }
-                else
-                {
-                    a.add(ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
-                }
-            }
-            a.add(1);
-            ans.add(a);
+            int num=(A+1-i)*ans.get(i-1)/i;
+            ans.add(num);
         }
-        return ans.get(A);
+        return ans;
     }
 }
