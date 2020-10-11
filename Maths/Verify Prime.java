05ff0cfca6265e4b3791384fb9fc66e69b56b5cb
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-10-11 21:53:17 +0530

    Add files via upload

---
+++ Maths/Verify Prime.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int isPrime(int A) 
+    {
+        if(A<=1)
+        {
+            return 0;
+        }
+        for(int i=2;i*i<=A;i++)
+        {
+            if(A%i==0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
