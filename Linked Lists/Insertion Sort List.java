ba74c13a23b5213b59e0e3f42cf3ca91199a836e
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2021-01-01 22:20:50 +0530

    Add files via upload

---
+++ Linked Lists/Insertion Sort List.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode A) 
+    {
+        if(A==null||A.next==null)
+        {
+            return A;
+        }
+        ListNode temp=A;
+        ListNode mid=A;
+        while(temp.next!=null&&temp.next.next!=null)
+        {
+            mid=mid.next;
+            temp=temp.next.next;
+        }
+        ListNode right=mid.next;
+        mid.next=null;
+        ListNode left=A;
+        ListNode l=insertionSortList(left);
+        ListNode r=insertionSortList(right);
+        ListNode t=merge(l,r);
+        return t;
+        
+    }
+    public ListNode merge(ListNode A, ListNode B)
+    {
+        if(A==null)
+        {
+            return B;
+        }
+        if(B==null)
+        {
+            return A;
+        }
+        if(A.val<B.val)
+        {
+            A.next=merge(A.next,B);
+            return A;
+        }
+        else
+        {
+            B.next=merge(A,B.next);
+            return B;
+        }
+    }
+}
