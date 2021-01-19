55e709cc92910ef368970cbcfccb2390b39aa2db
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2021-01-19 23:15:12 +0530

    Add files via upload

---
+++ Tree Data Structure/Max Depth of Binary Tree.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode A) 
+    {
+        if(A==null)
+        {
+            return 0;
+        }
+        return Math.max(maxDepth(A.left),maxDepth(A.right))+1;
+    }
+}
